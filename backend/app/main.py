b4de87f463f25387b600e5b8e7529d59c3eb234f
blibre4407 <blibre4407@gmail.com>
2026-05-07 13:02:53 +0100

    feat: implement PDF text extraction and multilingual vector generation

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from app.services.pdf_parser import extract_text_from_pdf # <-- Import your new service
-
+from app.services.nlp_engine import generate_vector
 app = FastAPI(title="TalentScan-AUF API")
 
 app.add_middleware(
@@ -30,15 +30,26 @@ async def upload_cv(file: UploadFile = File(...)):
     if not extracted_text:
         raise HTTPException(status_code=500, detail="Could not extract text. The PDF might be an image or encrypted.")
 
-    # 4. Return a preview (Later, this text will go into SQLite and FAISS)
+    # # 4. Return a preview (Later, this text will go into SQLite and FAISS)
+    # return {
+    #     "filename": file.filename,
+    #     "status": "Text extracted successfully",
+    #     "character_count": len(extracted_text),
+    #     "preview": extracted_text[:2133] + "..." # Send the first 250 characters as proof
+    # }
+
+# --- NEW: AI Vectorization ---
+    # Convert the text into a mathematical vector
+    vector = generate_vector(extracted_text)
+
     return {
         "filename": file.filename,
-        "status": "Text extracted successfully",
+        "status": "Text extracted and vectorized successfully",
         "character_count": len(extracted_text),
-        "preview": extracted_text[:2133] + "..." # Send the first 250 characters as proof
+        "vector_dimensions": len(vector), # Should output 384
+        "vector_preview": vector[:5] # Show the first 5 numbers of the array
     }
-
-
+    
 @app.post("/search")
 async def search_experts(job_description: str):
     # Phase 2: Logic for Sentence-Transformers & FAISS goes here
